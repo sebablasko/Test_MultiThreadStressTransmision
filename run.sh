894a1816451647072489e8959e57076762391f64
Sebastian <sebablasko@gmail.com>
2015-08-26 16:16:36 -0300

    actualizados paarametros de las pruebas

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 START=$(date +%s)
-res_dir=RESULTS
+echo $START
 
+res_dir=RESULTS
 mkdir $res_dir
+
+MAX_PACKS=1000000
+repetitions=50
+total_num_threads="1 2 4 8 16 24 32 48 64"
 
 echo ""
 echo "Iniciando pruebas..."
@@ -10,51 +15,55 @@ echo "Iniciando pruebas..."
 # ./run.sh {TOTAL_PAQUETES} {TOTAL_REPETICIONES} {[LISTA CANTIDAD THREADS]}
 
 echo ""
-echo "Prueba UDP"
-cd UDP
-./run.sh 1000000 20 1 2 4 8 16 32 64
-echo "Done!"
-cd ..
-
-echo ""
-echo "Prueba UNIX"
-cd UNIX
-./run.sh 1000000 20 1 2 4 8 16 32 64
-echo "Done!"
-cd ..
-
-echo ""
-echo "Prueba FIFO"
-cd FIFO
-./run.sh 1000000 20 1 2 4 8 16 32 64
-echo "Done!"
-cd ..
-
-echo ""
 echo "Prueba DEV_NULL"
 cd DEV_NULL
-./run.sh 1000000 20 1 2 4 8 16 32 64
+./run.sh $MAX_PACKS $repetitions $total_num_threads
 echo "Done!"
 cd ..
 
 echo ""
 echo "Prueba DEV_URANDOM"
 cd DEV_URANDOM
-./run.sh 1000000 20 1 2 4 8 16 32 64
+./run.sh $MAX_PACKS $repetitions $total_num_threads
+echo "Done!"
+cd ..
+
+echo ""
+echo "Prueba UDP"
+cd UDP
+./run.sh $MAX_PACKS $repetitions $total_num_threads
+echo "Done!"
+cd ..
+
+echo ""
+echo "Prueba UNIX"
+cd UNIX
+./run.sh $MAX_PACKS $repetitions $total_num_threads
 echo "Done!"
 cd ..
 
 echo ""
 echo "Prueba TCP"
 cd TCP
-./run.sh 1000000 20 1 2 4 8 16 32 64
+./run.sh $MAX_PACKS $repetitions $total_num_threads
 echo "Done!"
 cd ..
+
+echo ""
+echo "Prueba FIFO"
+cd FIFO
+./run.sh $MAX_PACKS $repetitions $total_num_threads
+echo "Done!"
+cd ..
+
 
 END=$(date +%s)
 DIFF=$(( $END - $START ))
 
 echo "El total de pruebas duro: $DIFF segundos"
+
+
+
 
 echo "Uniendo datos..."
 cd RESULTS
